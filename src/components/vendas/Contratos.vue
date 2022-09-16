b4867cf7f2494f4f829e8fc155a5f839ef3e2913
Fernando Andrade <fernandow.and@gmail.com>
2022-09-16 16:42:20 -0300

    258 ate 260. Query Params via navegação programática

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -6,19 +6,33 @@
         <div class="row">
           <div class="col-6">
             <label class="form-label">ID Contrato:</label>
-            <input type="text" class="form-control" />
+            <input
+              type="text"
+              class="form-control"
+              v-model="formPesquisa.id_like"
+            />
           </div>
           <div class="col-6">
             <label class="form-label">Data início:</label>
             <div class="input-group">
-              <input type="date" class="form-control" />
-              <input type="date" class="form-control" />
+              <input
+                type="date"
+                class="form-control"
+                v-model="formPesquisa.data_inicio_gte"
+              />
+              <input
+                type="date"
+                class="form-control"
+                v-model="formPesquisa.data_inicio_lte"
+              />
             </div>
           </div>
         </div>
       </div>
       <div class="card-footer">
-        <button type="button" class="btn btn-primary">Pesquisar</button>
+        <button type="button" class="btn btn-primary" @click="pesquisar">
+          Pesquisar
+        </button>
       </div>
     </div>
 
@@ -52,7 +66,26 @@ export default {
   mixins: [ApiMixin],
   data: () => ({
     parametrosDeRelacionamento: "_expand=lead&_expand=servico",
+    formPesquisa: {
+      id_like: "",
+      data_inicio_gte: "",
+      data_inicio_lte: "",
+    },
   }),
+  methods: {
+    pesquisar() {
+      console.log(this.formPesquisa);
+
+      Object.keys(this.formPesquisa).forEach((chave) => {
+        if (this.formPesquisa[chave] == "") delete this.formPesquisa[chave];
+      });
+
+      const queryParams = new URLSearchParams(this.formPesquisa).toString();
+      console.log(queryParams);
+      const url = `http://localhost:3000/contratos?${this.parametrosDeRelacionamento}&${queryParams}`;
+      this.getDadosApi(url);
+    },
+  },
   created() {
     console.log(this.$route);
     const queryParams = new URLSearchParams(this.$route.query).toString();
