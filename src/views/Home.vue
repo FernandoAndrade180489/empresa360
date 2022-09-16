c03950feea14ddf86852f51bf3c40b87fc6ff7fd
Fernando Andrade <fernandow.and@gmail.com>
2022-09-16 11:11:39 -0300

    229. Estilizando rota ativa parte 2 (active-class e exact-active-class)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,12 +15,17 @@
 
     <div class="d-flex">
       <div class="list-group menu-esquerda">
-        <router-link class="list-group-item list-group-item-action" to="/home">
+        <router-link
+          class="list-group-item list-group-item-action"
+          to="/home"
+          exact-active-class="active"
+        >
           <i class="bi bi-speedometer2"></i> Dashboard
         </router-link>
         <router-link
           class="list-group-item list-group-item-action"
           to="/home/vendas"
+          exact-active-class="active"
         >
           <i class="bi bi-cart"></i> Vendas
         </router-link>
@@ -28,12 +33,14 @@
         <router-link
           class="list-group-item list-group-item-action"
           to="/home/vendas/leads"
+          exact-active-class="active"
         >
           <i class="bi bi-caret-right"></i> Leads
         </router-link>
         <router-link
           class="list-group-item list-group-item-action"
           to="/home/vendas/contratos"
+          exact-active-class="active"
         >
           <i class="bi bi-caret-right"></i> Contratos
         </router-link>
@@ -41,6 +48,7 @@
         <router-link
           class="list-group-item list-group-item-action"
           to="/home/servicos"
+          exact-active-class="active"
         >
           <i class="bi bi-card-checklist"></i> Serviços
         </router-link>
@@ -88,9 +96,15 @@ export default {
   background-color: #65a1fa;
   border-color: #65a1fa;
 } */
-.router-link-exact-active {
+/* .router-link-exact-active {
   color: #fff;
   background-color: #0d6efd;
   border-color: #0d6efd;
-}
+} */
+
+/* .ativo {
+  color: #fff;
+  background-color: #f00;
+  border-color: #f00;
+} */
 </style>
